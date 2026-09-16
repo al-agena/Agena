cf4867a988de259665a656b634df995b5caaf155
al-agena <al-agena@t-online.de>
2026-09-16 23:39:27 +0200

    Add files via upload

--- installers/WarpIN/build.cmd
+++ installers/WarpIN/build.cmd
@@ -1,3 +1,3 @@
 /**/
 'cd c:\ecs\install\warpin'
-'wic -a c:\agena-7.10.2-os2.wpi 1 -c"c:\build" -r * -s c:\agena\installers\WarpIN\agena.wis'
+'wic -a c:\agena-7.10.3-os2.wpi 1 -c"c:\build" -r * -s c:\agena\installers\WarpIN\agena.wis'
